c661cbdff8ef73e2b9114aa4f65f89f62fa3ab73
Sasi9849 <137712685+Sasi9849@users.noreply.github.com>
2024-02-19 09:27:47 +0530

    Delete shellscript directory

--- shellscript/script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if[[ $# -ne 2]];
-	echo "you didnt enter exact 2"
-	echo "usage :$0<f1><f2>"
-	echo $@
-	echo"$@"
-	exit 1
-fi
--- shellscript/script1.sh
+++
@@ -1,8 +0,0 @@
-#b/bin/bash
-read -t 5 -p "input your name" name
-read -t 5 -p "input your age" age
-read -t 5 -p "input your town" town
-echo "my name is $name"
-echo "my name is $age"
-echo "im from $town"
-exit 0
